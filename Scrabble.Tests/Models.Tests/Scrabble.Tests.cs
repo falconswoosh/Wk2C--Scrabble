6059ff80ba1a4e8770247874d8e7dd5fe1513935
Alan Falcon and Rakhee Gandhi <alan+rakhee@>
2017-10-19 16:13:27 -0700

    Fix TestMethod1 AGAIN.

--- Scrabble.Tests/Models.Tests/Scrabble.Tests.cs
+++ Scrabble.Tests/Models.Tests/Scrabble.Tests.cs
@@ -9,17 +9,23 @@ namespace Scrabble.Tests
   [TestClass]
   public class ScrabbleTest
 {
-    [TestMethod]
-    public void Generate_PlayerReceives7RandomLetters_True()
-    {
-      ScrabbleScorer testScrabble = new ScrabbleScorer();
-      char result = testScrabble.Generate('d');
-      // Console.WriteLine("result: "+result);
-      Assert.IsInstanceOfType(char, typeof(result));
-      // Assert.AreEqual(true, testScrabble.Generate());
-      // Assert.AreEqual(true, testRandomNumbers.Generate("HijKlmN"));
-      // Assert.AreEqual(true, testRandomNumbers.Generate("oPqrStu"));
-    }
+  [TestMethod]
+  public void GetRandomChar_ProgramGeneratesOneRandomLetter_True()
+  {
+     ScrabbleScorer testScrabble = new ScrabbleScorer();
+     char result = testScrabble.GetRandomChar('d');
+     Console.WriteLine("result: "+result);
+     Assert.IsInstanceOfType(result, typeof(char));
+   }
+    // [TestMethod]
+    // public void Generate_PlayerReceives7RandomLetters_True()
+    // {
+    //   ScrabbleScorer testScrabble = new ScrabbleScorer();
+    //   List<string> result = testScrabble.Generate();
+    //   Console.WriteLine("result: "+result);
+    //   Assert.IsInstanceOfType(List<string>, typeof(result));
+    //
+    // }
     //
     // [TestMethod]
     // public void Input_PlayerWillInputAWord_Input()
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -8,13 +8,27 @@ namespace Scrabble.Models
   {
     static Random _random = new Random();
 
-    public char Generate(char c)
+    public char GetRandomChar(char c)
     {
       int num = _random.Next(0, 26);
       char letter = (char)('a' + num);
-
       // Console.WriteLine(letter);
       return letter;
     }
+
+    // public List<string> Generate()
+    // {
+    //   List<string> initialLetters = new List<string> {};
+    //   for (int i = 1; i < 8; i++)
+    //   {
+    //
+    //     int num = _random.Next(0, 26);
+    //
+    //     string letter = (string)('a' + num.ToString());
+    //     initialLetters.Add(letter);
+    //   }
+    //   // Console.WriteLine(letter);
+    //   return initialLetters;
+    // }
   }
 }
